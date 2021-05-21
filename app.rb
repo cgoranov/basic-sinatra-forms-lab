fecdd2670fc9e0944dda115416189a63cd718aef
Che <cgoranov@gmail.com>
2021-05-20 23:55:49 -0500

    commit tree 5fc5f8d42825502a45e2d60087b12d804f656aee

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        @name = params["name"]
+        @coach = params["coach"]
+        @pguard = params["pg"]
+        @sguard = params["sg"]
+        @sfoward = params["sf"]
+        @pforward = params["pf"]
+        @center = params["c"]
+        erb :team
+    end
+
 
 end
